d379627dcac61a6d273b8c534f4078968aec21c1
rashonwill <williams_rashon92@yahoo.com>
2022-11-25 23:37:06 -0600

    iffey

--- backend/api/redisclient.js
+++ backend/api/redisclient.js
@@ -7,10 +7,12 @@ let redisClient = redis.createClient({
   },
 });
 
-redisClient.on("error", (err) => {
-  console.log("Redis Client Error", err);
-});
-redisClient.on("ready", () => console.log("Redis is ready"));
-await redis.connect();
+(async () => {
+  redisClient.on("error", (err) => {
+    console.log("Redis Client Error", err);
+  });
+  redisClient.on("ready", () => console.log("Redis is ready"));
+  await redis.connect();
+})();
 
 module.exports = redisClient;
