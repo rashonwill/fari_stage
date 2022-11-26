4584c75f96727be6be7a18e370544524ba4f8f00
rashonwill <williams_rashon92@yahoo.com>
2022-11-25 18:20:08 -0600

    redis io changes

--- backend/api/redisclient.js
+++ backend/api/redisclient.js
@@ -1,8 +1,9 @@
 const Redis = require("ioredis");
 const redis = new Redis({
   host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
+  tls: {
+    host: process.env.REDIS_HOST,
+  },
 });
 
 module.exports = redis;
