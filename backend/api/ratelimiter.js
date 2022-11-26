5f06498db8593ec2914e972b4306d4d68533623f
rashonwill <williams_rashon92@yahoo.com>
2022-11-25 22:11:22 -0600

    combined file

--- backend/api/ratelimiter.js
+++ backend/api/ratelimiter.js
@@ -1,12 +1,21 @@
-const redis = require("./redisclient");
+// const redis = require("./redisclient");
+
+const redis = require("redis");
+let redisClient = redis.createClient({
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true,
+    rejectUnauthorized: false,
+  },
+});
 
 (async () => {
-  redis.on("error", (err) => {
+  redisClient.on("error", (err) => {
     console.log("Redis Client Error", err);
   });
-  redis.on("ready", () => console.log("Redis is ready"));
-  await redis.connect();
-  await redis.set("App", "Hello Fari APP");
+  redisClient.on("ready", () => console.log("Redis is ready"));
+  await redisClient.connect();
+  await redisClient.set("App", "Hello Fari APP");
   const myapp = await redis.get("App");
   console.log("Redis key value", myapp);
 })();
@@ -15,13 +24,13 @@ function rateLimiter({ secondsWindow, allowedHits }) {
   return async function (req, res, next) {
     const ip = req.headers["x-forwared-for"] || req.connection.remoteAddress;
     console.log("requesting IP", ip);
-    const requests = await redis.incr(ip);
+    const requests = await redisClient.incr(ip);
     console.log("req redis incr", requests);
     let ttl;
     if (requests === 1) {
       ttl = secondsWindow;
     } else {
-      ttl = await redis.ttl(ip);
+      ttl = await redisClient.ttl(ip);
     }
 
     if (requests > allowedHits) {
