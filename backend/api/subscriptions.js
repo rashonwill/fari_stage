7b07e248a20b2219ef7fe5927a6c4ccd000706da
rashonwill <75400387+rashonwill@users.noreply.github.com>
2022-11-30 13:04:31 -0600

    Create subscriptions.js

---
+++ backend/api/subscriptions.js
@@ -0,0 +1,189 @@
+const express = require("express");
+const subscriptionsRouter = express.Router();
+const { requireUser } = require("./utils");
+const { body, check, validationResult } = require("express-validator");
+const rateLimiter = require("./ratelimiter");
+
+
+const {
+  registerVendor,
+  setStripeID,
+  updateVendorSubscription,
+  updateUserSubscription,
+  
+} = require("../db");
+
+
+//Set Vendor Registered to True
+
+subscriptionsRouter.patch("/vendor-registration/:id", requireUser, check('id').not().isEmpty().isNumeric().withMessage('Not a valid value').trim().escape(), async (req, res, next) => {
+const { id } = req.params;
+ let errors = validationResult(req);  
+ if (!errors.isEmpty()) {
+    return res.status(400).send({name: 'Validation Error', message: errors.array()[0].msg});
+}else{  
+try {
+const verified = await registerVendor(id);
+ res.send({vendor: verified});
+}catch(error){
+console.log('Oops could register vendor', error)
+next({ name: "ErrorRegisteringVendor", message: "Could not complete vendor registration" });
+}
+}
+});
+
+
+//Set Vendor Flag on Users table
+
+subscriptionsRouter.patch("/vendor-subscription-update/:id", requireUser, check('id').not().isEmpty().isNumeric().withMessage('Not a valid value').trim().escape(), async (req, res, next) => {
+const { id } = req.params;
+   let errors = validationResult(req);
+     if (!errors.isEmpty()) {
+   return res.status(400).send({name: 'Validation Error', message: errors.array()[0].msg});
+}else{ 
+try{
+ const updatingVendor = await updateVendorSubscription(id);
+ res.send({updatedSubscription: updatingVendor})
+}catch(error){
+console.log('Oops, could not update vendor subscription status', error);
+}
+}
+});
+
+//Set user subcription flag on Users table
+
+subscriptionsRouter.patch("/user-subscription-update/:id", requireUser, check('id').not().isEmpty().isNumeric().withMessage('Not a valid value').trim().escape(), async (req, res, next) => {
+const { id } = req.params;
+  let errors = validationResult(req);
+     if (!errors.isEmpty()) {
+   return res.status(400).send({name: 'Validation Error', message: errors.array()[0].msg});
+}else{	
+try{
+ const updatingUser = await updateUserSubscription(id);
+ res.send({updatedSubscription: updatingUser})
+}catch(error){
+console.log('Oops, could not update user subscription status', error);
+}
+}
+});
+
+//Set Vendor Stripe Account ID On Vendor Table
+
+subscriptionsRouter.patch("/setstripe-acct/:id", requireUser, check('id').not().isEmpty().isNumeric().withMessage('Not a valid value').trim().escape(), async (req, res, next) => {
+const { id } = req.params;
+ 
+const stripe_acctid = req.body.stripe_acctid; 
+const stripeAcct = {
+stripe_acctid: stripe_acctid,
+}
+ let errors = validationResult(req);  
+ if (!errors.isEmpty()) {
+    return res.status(400).send({name: 'Validation Error', message: errors.array()[0].msg});
+}else{ 
+try {
+const verifiedAcct = await setStripeID(id, stripeAcct);
+ res.send({vendor: verifiedAcct});
+}catch(error){
+console.log('Oops could register vendor', error)
+next({ name: "ErrorRegisteringVendor", message: "Could not set vendor stripe account" });
+} 
+}
+});
+
+//Stripe Connect
+
+
+subscriptionsRouter.post("/onboard-user", requireUser, async (req, res) => {
+ try{
+ const account = await stripe.accounts.create({
+ type: 'standard',
+});
+  const accountLink = await stripe.accountLinks.create({
+  account: account.id,
+  refresh_url: 'https://fari-test.netlify.app/vendor-registration',
+  return_url: 'https://fari-test.netlify.app/vendor-registration',
+  type: 'account_onboarding',
+});
+  
+  console.log(accountLink, account.id)
+ res.json({url: accountLink.url, accountid: account.id })
+ }catch(error){
+  console.log(error)
+ }
+ 
+});
+
+
+//Monthly Subscriptions
+
+
+subscriptionsRouter.post("/vendor-subscription", requireUser, async function(req, res) {
+const priceID = 'price_1L1BVrF7h5B228czlK6zy2db'
+const taxRateId = 'txr_1L19ISF7h5B228czIjPOqQmS'
+const fariVendorID = req.body.vendorid
+ try{
+  const session = await stripe2.checkout.sessions.create({
+  mode: 'subscription',
+  subscription_data: {
+    default_tax_rates: [
+      taxRateId
+    ],
+  },
+  payment_intent_data: {
+    metadata: {
+     FariVendorID: fariVendorID,
+    }
+  },
+  line_items: [
+    {
+      price: priceID,
+      quantity: 1,
+    },
+  ],
+  success_url: 'https://fari-test.netlify.app/registration-complete?session_id={CHECKOUT_SESSION_ID}',
+  cancel_url: 'https://fari-test.netlify.app/vendor-registration',
+   
+});  
+res.json({url: session.url })             
+ }catch(error){
+ console.log(error)
+}              
+                 
+})
+
+
+subscriptionsRouter.post("/fariplus-subscription", requireUser, async function(req, res){
+const priceID = 'price_1L19HEF7h5B228czDoDCwbJp'
+const taxRateId = 'txr_1L19ISF7h5B228czIjPOqQmS'
+const fariUserID = req.body.userid
+ try{
+  const session = await stripe2.checkout.sessions.create({
+  mode: 'subscription',
+  subscription_data: {
+    default_tax_rates: [
+      taxRateId
+    ],
+  },
+  payment_intent_data: {
+    metadata: {
+     FariUserID: fariUserID,
+    }
+  },
+  line_items: [
+    {
+      price: priceID,
+      quantity: 1,
+    },
+  ],
+  success_url: 'https://fari-test.netlify.app/subscription-complete?session_id={CHECKOUT_SESSION_ID}',
+  cancel_url: 'https://fari-test.netlify.app/settings',
+  
+}); 
+res.json({url: session.url })             
+ }catch(error){
+ console.log(error)
+}              
+                 
+})
+
+module.exports = subscriptionsRouter;
