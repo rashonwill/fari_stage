90db5a21b57c772a6e238b43d0852fc36bf673c2
rashonwill <75400387+rashonwill@users.noreply.github.com>
2022-12-07 15:01:56 -0600

    Update redisclient.js

--- backend/api/redisclient.js
+++ backend/api/redisclient.js
@@ -1,6 +1,7 @@
 const redis = require("redis");
 let redisClient = redis.createClient({
   url: process.env.REDIS_URL,
+  connectTimeout: 10000,
   socket: {
     tls: true,
     rejectUnauthorized: false,
