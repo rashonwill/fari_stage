1bff74842427605bf6f1f88a10cda3a31cc051fd
rashonwill <75400387+rashonwill@users.noreply.github.com>
2022-12-07 14:57:58 -0600

    Update ratelimiter.js

--- backend/api/ratelimiter.js
+++ backend/api/ratelimiter.js
@@ -5,6 +5,7 @@ const renderRedis = new Redis({
   host: process.env.REDIS_HOST, // Render Redis hostname, REGION-redis.render.com
   password: process.env.REDIS_PASSWORD, // Provided password
   port: process.env.REDIS_PORT || 6379, // Connection port
+  connectTimeout: 10000,
   tls: {
     rejectUnauthorized: false,
   },
