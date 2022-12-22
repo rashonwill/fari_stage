0c7da52136abc33765b4d676a86598b0bafc524f
rashonwill <williams_rashon92@yahoo.com>
2022-12-22 15:21:24 -0600

    extraction

--- backend/api/account.js
+++ backend/api/account.js
@@ -187,7 +187,7 @@ accountRouter.post(
     } else {
       try {
         const refreshToken = await getUserToken(username);
-        res.json({ refresher: refreshToken });
+        let refresher = res.json(refreshToken.jwt_token);
         console.log(refresher);
         if (!refreshToken || refreshToken === null) {
           res.status(401).json({ message: "No token found" });
