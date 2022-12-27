6cb6d9840e739de15178a9b87d9c74d38980d8bc
rashonwill <75400387+rashonwill@users.noreply.github.com>
2022-12-27 11:47:12 -0600

    Update theater.js

--- public/theater.js
+++ public/theater.js
@@ -745,7 +745,6 @@ async function getMyFavs() {
       },
     });
     const data = await response.json();
-	  console.log(data)
     if (data.myFavVids.length > 0) {
       $(".fav-videos").empty();
       $(".newUserMessage-favs message").css("display", "none");
@@ -817,7 +816,6 @@ async function getWatchList() {
       },
     });
     const data = await response.json();
-	  console.log(data)
     if (data.myWatchList.length > 0) {
       $(".later-videos").empty();
       $(".newUserMessage-watchlater message").css("display", "none");
