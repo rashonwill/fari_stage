3732a299412772a9d755e3f120d2bb07b4ac86f5
rashonwill <75400387+rashonwill@users.noreply.github.com>
2023-01-07 10:07:37 -0600

    Update orders.js

--- backend/api/orders.js
+++ backend/api/orders.js
@@ -98,30 +98,30 @@ ordersRouter.post(
 
 
 
-ordersRouter.post('/webhook', express.raw({type: "application/json"}), async (request, response) => {
-  const sig = request.headers['stripe-signature'];
-  const payload = request.body
-  console.log('sig', sig)
-  console.log('payload', payload)
-  console.log('secret', WEBHOOK_SECRET);
-  let event;
-   try {
-    event = stripe.webhooks.constructEvent(payload, sig, WEBHOOK_SECRET);
-  } catch (err) {
-    console.log(err)
-    return response.status(400).send(`Webhook Error: ${err.message}`);
-  }
+// ordersRouter.post('/webhook', express.raw({type: "application/json"}), async (request, response) => {
+//   const sig = request.headers['stripe-signature'];
+//   const payload = request.body
+//   console.log('sig', sig)
+//   console.log('payload', payload)
+//   console.log('secret', WEBHOOK_SECRET);
+//   let event;
+//    try {
+//     event = stripe.webhooks.constructEvent(payload, sig, WEBHOOK_SECRET);
+//   } catch (err) {
+//     console.log(err)
+//     return response.status(400).send(`Webhook Error: ${err.message}`);
+//   }
 
-  // Handle the checkout.session.completed event
-  if (event.type === 'checkout.session.completed') {
-    const session = event.data.object;
+//   // Handle the checkout.session.completed event
+//   if (event.type === 'checkout.session.completed') {
+//     const session = event.data.object;
 
-    // Fulfill the purchase...
-    console.log(session)
-  }
+//     // Fulfill the purchase...
+//     console.log(session)
+//   }
 
-  response.status(200);
-})
+//   response.status(200);
+// })
 
 
 
