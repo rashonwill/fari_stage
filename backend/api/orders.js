6c70161961fbf59dbf657c8462aa11707f373749
rashonwill <75400387+rashonwill@users.noreply.github.com>
2023-01-06 22:38:15 -0600

    Update orders.js

--- backend/api/orders.js
+++ backend/api/orders.js
@@ -97,7 +97,7 @@ ordersRouter.post(
 
 ordersRouter.post('/webhooks/fari', express.raw({ type: 'application/json' }), async (request, response) => {
   const sig = request.headers['stripe-signature'];
-  const payload = JSON.parse(request.body)
+  const payload = JSON.stringify(request.body)
   console.log('sig', sig)
   console.log('payload', payload)
    let event;
