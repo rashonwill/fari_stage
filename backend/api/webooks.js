8871e950f0d6f5478b1560189b91e3da61df32f2
rashonwill <75400387+rashonwill@users.noreply.github.com>
2023-01-07 10:22:03 -0600

    Create webooks.js

---
+++ backend/api/webooks.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const { WEBHOOK_SECRET } = process.env;
+
+app.get("/webhook", async (req, res) => {
+  res.send("Welcome to Stripe webhooks");
+});
+
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  async (request, response) => {
+    const sig = request.headers["stripe-signature"];
+    const payload = request.body;
+    console.log("sig", sig);
+    console.log("payload", payload);
+    console.log("secret", WEBHOOK_SECRET);
+    let event;
+    try {
+      event = stripe.webhooks.constructEvent(payload, sig, WEBHOOK_SECRET);
+    } catch (err) {
+      console.log(err);
+      return response.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    // Handle the checkout.session.completed event
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+
+      // Fulfill the purchase...
+      console.log("fulfilling order now");
+      console.log(session);
+    }
+
+    response.status(200);
+  }
+);
