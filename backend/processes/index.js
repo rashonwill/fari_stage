fe6f88f68778ab7dbe72934adc409cfc8377b09f
rashonwill <williams_rashon92@yahoo.com>
2023-01-07 21:50:17 -0600

    processors route modifications

--- backend/processes/index.js
+++ backend/processes/index.js
@@ -2,6 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const processesRouter = express.Router();
 
+const cors = require("cors");
+processesRouter.use(cors({ origin: "*" }));
+
+processesRouter.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", process.env.ORIGIN_URL);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Request-With, Content-Type, Accept, Authorization, Cookies"
+  );
+  next();
+});
+
 const webhookRouter = require("./webhooks");
 processesRouter.use("/webhooks", webhookRouter);
 
