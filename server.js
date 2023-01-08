d50d2bd4703ba1892f606f6857ba6cd93c15bd70
rashonwill <williams_rashon92@yahoo.com>
2023-01-07 20:16:12 -0600

    webhook back to serverjs

--- server.js
+++ server.js
@@ -23,7 +23,36 @@ server.use(helmet());
 const cors = require("cors");
 server.use(cors({ origin: "*" }));
 
-server.use("/processes", require("./backend/processes"));
+server.get("/webhook", async (req, res) => {
+  res.send("Welcome to Stripe webhooks");
+});
+
+server.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  async (request, response) => {
+    const sig = request.headers["stripe-signature"];
+    const payload = request.body;
+    let event;
+    try {
+      event = stripe.webhooks.constructEvent(payload, sig, WEBHOOK_SECRET);
+    } catch (err) {
+      console.log(err);
+      return response.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    // Handle the checkout.session.completed event
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+
+      // Fulfill the purchase...
+      console.log("fulfilling order now");
+      console.log(session);
+    }
+
+    response.status(200);
+  }
+);
 
 //middleware
 
