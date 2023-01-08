ce35b66d2d92da42748556929459c4f9d681dc19
rashonwill <williams_rashon92@yahoo.com>
2023-01-07 19:21:16 -0600

    hook moved to serverjs

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const http = require("http");
 const os = require("os");
 const helmet = require("helmet");
 const numCPU = os.cpus().length;
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const { WEBHOOK_SECRET } = process.env;
 
 const client = require("./backend/db/client");
 client.connect();
@@ -48,6 +50,36 @@ const morgan = require("morgan");
 server.use(morgan("dev"));
 
 server.use("/api", require("./backend/api"));
+
+server.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  async (request, response) => {
+    const sig = request.headers["stripe-signature"];
+    const payload = request.body;
+    console.log("sig", sig);
+    console.log("payload", payload);
+    console.log("secret", WEBHOOK_SECRET);
+    let event;
+    try {
+      event = stripe.webhooks.constructEvent(payload, sig, WEBHOOK_SECRET);
+    } catch (err) {
+      console.log(err);
+      return response.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    // Handle the checkout.session.completed event
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+
+      // Fulfill the purchase...
+      console.log("fulfilling order now");
+      console.log(session);
+    }
+
+    response.status(200);
+  }
+);
 
 if (cluster.isMaster) {
   for (let index = 0; index < numCPU; index++) {
