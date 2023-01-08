2b845c43b9f2e95191552b3422629643fa5a5ce1
rashonwill <williams_rashon92@yahoo.com>
2023-01-07 20:10:50 -0600

    processes error

--- backend/processes/index.js
+++ backend/processes/index.js
@@ -1,10 +1,9 @@
 require("dotenv").config();
-
 const express = require("express");
 const processesRouter = express.Router();
 
 const webhookRouter = require("./webhooks");
-apiRouter.use("/webhooks", webhookRouter);
+processesRouter.use("/webhooks", webhookRouter);
 
 processesRouter.use((error, req, res, next) => {
   res.send(error);
