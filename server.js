598da9dd21244603b945aebc94ffe2dbfe097c07
rashonwill <williams_rashon92@yahoo.com>
2023-01-07 19:47:56 -0600

    move hook up

--- server.js
+++ server.js
@@ -18,6 +18,35 @@ client.connect();
 const hpp = require("hpp");
 server.use(hpp());
 
+server.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  async (request, response) => {
+    const sig = request.headers["stripe-signature"];
+    const payload = request.body;
+    console.log("sig", sig);
+    console.log("payload", payload);
+    console.log("secret", WEBHOOK_SECRET);
+    let event;
+    try {
+      event = stripe.webhooks.constructEvent(payload, sig, WEBHOOK_SECRET);
+    } catch (err) {
+      console.log(err);
+      return response.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    // Handle the checkout.session.completed event
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+
+      // Fulfill the purchase...
+      console.log("fulfilling order now");
+    }
+
+    response.status(200);
+  }
+);
+
 server.use(express.static("public", { extensions: ["html"] }));
 server.use(express.urlencoded({ extended: false, limit: "1kb" }));
 server.use(express.json({ limit: "100mb" }));
@@ -55,36 +84,6 @@ server.use("/api", require("./backend/api"));
 server.get("/webhook", async (req, res) => {
   res.send("Welcome to Stripe webhooks");
 });
-
-server.post(
-  "/webhook",
-  express.raw({ type: "application/json" }),
-  async (request, response) => {
-    const sig = request.headers["stripe-signature"];
-    const payload = request.body;
-    console.log("sig", sig);
-    console.log("payload", payload);
-    console.log("secret", WEBHOOK_SECRET);
-    let event;
-    try {
-      event = stripe.webhooks.constructEvent(payload, sig, WEBHOOK_SECRET);
-    } catch (err) {
-      console.log(err);
-      return response.status(400).send(`Webhook Error: ${err.message}`);
-    }
-
-    // Handle the checkout.session.completed event
-    if (event.type === "checkout.session.completed") {
-      const session = event.data.object;
-
-      // Fulfill the purchase...
-      console.log("fulfilling order now");
-      console.log(session);
-    }
-
-    response.status(200);
-  }
-);
 
 if (cluster.isMaster) {
   for (let index = 0; index < numCPU; index++) {
