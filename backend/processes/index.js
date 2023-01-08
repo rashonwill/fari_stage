78150956303b9aad55a5484dd0830e04be2c7d87
rashonwill <williams_rashon92@yahoo.com>
2023-01-07 20:25:47 -0600

    webhooks back to processes

--- backend/processes/index.js
+++ backend/processes/index.js
@@ -1,12 +1,12 @@
-// require("dotenv").config();
-// const express = require("express");
-// const processesRouter = express.Router();
+require("dotenv").config();
+const express = require("express");
+const processesRouter = express.Router();
 
-// const webhookRouter = require("./webhooks");
-// processesRouter.use("/webhooks", webhookRouter);
+const webhookRouter = require("./webhooks");
+processesRouter.use("/webhooks", webhookRouter);
 
-// processesRouter.use((error, req, res, next) => {
-//   res.send(error);
-// });
+processesRouter.use((error, req, res, next) => {
+  res.send(error);
+});
 
-// module.exports = processesRouter;
+module.exports = processesRouter;
--- server.js
+++ server.js
@@ -21,36 +21,7 @@ server.use(hpp());
 const cors = require("cors");
 server.use(cors({ origin: "*" }));
 
-server.get("/webhook", async (req, res) => {
-  res.send("Welcome to Stripe webhooks");
-});
-
-server.post(
-  "/webhook",
-  express.raw({ type: "application/json" }),
-  async (request, response) => {
-    const sig = request.headers["stripe-signature"];
-    const payload = request.body;
-    let event;
-    try {
-      event = stripe.webhooks.constructEvent(payload, sig, WEBHOOK_SECRET);
-    } catch (err) {
-      console.log(err);
-      return response.status(400).send(`Webhook Error: ${err.message}`);
-    }
-
-    // Handle the checkout.session.completed event
-    if (event.type === "checkout.session.completed") {
-      const session = event.data.object;
-
-      // Fulfill the purchase...
-      console.log("fulfilling order now");
-      console.log(session);
-    }
-
-    response.status(200);
-  }
-);
+server.use("/processes", require("./backend/processes"));
 
 //middleware
 
